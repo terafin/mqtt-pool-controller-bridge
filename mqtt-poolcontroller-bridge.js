508e5c5d9486faa9726b6389b6b63276d59c5d35
terafin <jwood@me.com>
2019-02-24 09:14:40 -0800

    Fixing for API update

--- mqtt-poolcontroller-bridge.js
+++ mqtt-poolcontroller-bridge.js
@@ -2,8 +2,6 @@
 const mqtt = require('mqtt')
 const _ = require('lodash')
 const logging = require('homeautomation-js-lib/logging.js')
-const repeat = require('repeat')
-const bodyParser = require('body-parser')
 const io = require('socket.io-client')
 
 require('homeautomation-js-lib/mqtt_helpers.js')
@@ -17,11 +15,11 @@ var mqttOptions = {}
 var shouldRetain = process.env.MQTT_RETAIN
 
 if (_.isNil(shouldRetain)) {
-    shouldRetain = true
+	shouldRetain = true
 }
 
 if (!_.isNil(shouldRetain)) {
-    mqttOptions['retain'] = shouldRetain
+	mqttOptions['retain'] = shouldRetain
 }
 
 
@@ -29,86 +27,100 @@ var circuits = {}
 
 // Setup MQTT
 const client = mqtt.setupClient(function() {
-    client.subscribe(pool_topic + '/circuit/+/set')
-    client.subscribe(pool_topic + '/poolheat/+/set')
+	client.subscribe(pool_topic + '/circuit/+/set')
+	client.subscribe(pool_topic + '/poolheat/+/set')
 }, null)
 
 
 client.on('message', (topic, message) => {
-    logging.info(' ' + topic + ':' + message)
-    if ( topic.toString().includes('circuit')) {
-        const components = topic.split('/')
-        const circuit = components[components.length - 2]
-        logging.info(' set circuit: ' + circuit + '   to: ' + message)
+	logging.info(' ' + topic + ':' + message)
+	if ( topic.toString().includes('circuit')) {
+		const components = topic.split('/')
+		const circuit = components[components.length - 2]
+		logging.info(' set circuit: ' + circuit + '   to: ' + message)
 
         
-        Object.keys(circuits).forEach(circuitIndex => {
-            if ( circuitIndex == circuit ) {
-                const circuitInfo = circuits[circuitIndex]
-                const status = circuitInfo['status']
-                if ( status != message ) {
-                    socket.emit('toggleCircuit', circuitIndex)
-                }
-            }
-        })
-    }
-    else if ( topic.toString().includes('poolheat')) {
-        const components = topic.split('/')
-        const action = components[components.length - 2]
-        logging.info(' set heat action: ' + action + '   to: ' + message)
+		Object.keys(circuits).forEach(circuitIndex => {
+			if ( circuitIndex == circuit ) {
+				const circuitInfo = circuits[circuitIndex]
+				const status = circuitInfo['status']
+				if ( status != message ) {
+					socket.emit('toggleCircuit', circuitIndex)
+				}
+			}
+		})
+	} else if ( topic.toString().includes('poolheat')) {
+		const components = topic.split('/')
+		const action = components[components.length - 2]
+		logging.info(' set heat action: ' + action + '   to: ' + message)
 
         
-        if ( action.includes('setpoint')) {
-            socket.emit('setPoolSetPoint', message.toString())
-        } else if ( action.includes('mode')) {
-            socket.emit('poolheatmode', message.toString())
-        }
-    }
+		if ( action.includes('setpoint')) {
+			socket.emit('setPoolSetPoint', message.toString())
+		} else if ( action.includes('mode')) {
+			socket.emit('poolheatmode', message.toString())
+		}
+	}
 })
 
-function publishUpdate(category, index, value) {
-    var topic = pool_topic
+const publishUpdate = function(category, index, value) {
+	var topic = pool_topic
 
-    if ( !_.isNil(category))
-        topic = topic + '/' + category.toString()
-    if ( !_.isNil(index))
-        topic = topic + '/' + index.toString()
+	if ( !_.isNil(category)) {
+		topic = topic + '/' + category.toString()
+	}
+	if ( !_.isNil(index)) {
+		topic = topic + '/' + index.toString()
+	}
 
-    client.smartPublish(topic, value.toString(), mqttOptions)
+	if (_.isNil(value)) {
+		value = 0
+	}
+    
+	client.smartPublish(topic, value.toString(), mqttOptions)
 }
 
 
 const socket = io(poolHost)
 
 socket.on('connect', () => {
-    logging.info('connected to pool host')
+	logging.info('connected to pool host')
 })
 
 socket.on('circuit', (circuitUpdate) => {
-    circuits = circuitUpdate
+	circuits = circuitUpdate
+	logging.info('circuit: ' + JSON.stringify(circuitUpdate))
+	circuits = circuits['circuit']
 
-    Object.keys(circuits).forEach(circuitIndex => {
-        const circuitInfo = circuits[circuitIndex]
-        publishUpdate('circuit', circuitIndex, circuitInfo['status'])
-    })
+	Object.keys(circuits).forEach(circuitIndex => {
+		const circuitInfo = circuits[circuitIndex]
+		publishUpdate('circuit', circuitIndex, circuitInfo['status'])
+	})
 
 })
 socket.on('pump', (pumps) => {
-    Object.keys(pumps).forEach(pumpIndex => {
-        const pumpInfo = pumps[pumpIndex]
-        const power = pumpInfo['power']
-        publishUpdate('pump', pumpIndex, power)
-    })
+	logging.info('found pumps: ' + JSON.stringify(pumps))
+	pumps = pumps['pump']
+    
+	Object.keys(pumps).forEach(pumpIndex => {
+		const pumpInfo = pumps[pumpIndex]
+		const power = pumpInfo['power']
+		publishUpdate('pump', pumpIndex, power)
+	})
 })
 socket.on('chlorinator', (chlorinator) => {
-    logging.info('found chlorinator: ' + JSON.stringify(chlorinator))
-    Object.keys(chlorinator).forEach(chlorinatorKey => {
-        publishUpdate('chlorinator', chlorinatorKey, chlorinator[chlorinatorKey])
-    })
+	logging.info('found chlorinator: ' + JSON.stringify(chlorinator))
+	chlorinator = chlorinator['chlorinator']
+
+	Object.keys(chlorinator).forEach(chlorinatorKey => {
+		publishUpdate('chlorinator', chlorinatorKey, chlorinator[chlorinatorKey])
+	})
 })
 socket.on('temperatures', (temperatures) => {
-    logging.info('found temperatures: ' + JSON.stringify(temperatures))
-    Object.keys(temperatures).forEach(temperature => {
-        publishUpdate('temperature', temperature, temperatures[temperature])
-    })
+	logging.info('found temperatures: ' + JSON.stringify(temperatures))
+	temperatures = temperatures['temperature']
+    
+	Object.keys(temperatures).forEach(temperature => {
+		publishUpdate('temperature', temperature, temperatures[temperature])
+	})
 })
