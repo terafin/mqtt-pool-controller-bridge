e8b9e0541bb1786fe4f79e9d1eec81aa79ebeb91
Justin Wood <jwood@me.com>
2020-09-18 09:42:31 -0700

    Pushing running average down

--- mqtt-poolcontroller-bridge.js
+++ mqtt-poolcontroller-bridge.js
@@ -4,6 +4,7 @@ const _ = require('lodash')
 const logging = require('homeautomation-js-lib/logging.js')
 const io = require('socket.io-client')
 const mqtt_helpers = require('homeautomation-js-lib/mqtt_helpers.js')
+const utilities = require('homeautomation-js-lib/utilities.js')
 const got = require('got')
 
 // Config
@@ -96,61 +97,23 @@ const publishUpdate = function(category, index, value) {
 }
 
 
-var running_average_map = {}
-var values_thrown_away_counter = {}
-
 const VALUES_FOR_RUNNING_AVERAGE = 8
 const MIN_VALUES_FOR_RUNNING_AVERAGE_THRESHOLD = 3
 const THRESHOLD_TO_THROW_AWAY = 6
 const MAX_VALUES_TO_THROW_AWAY = 2
 
-const add_running_average = function(key, value) {
-    var values = running_average_map[key]
-    if (_.isNil(values)) {
-        values = []
-        values_thrown_away_counter[key] = 0
-    }
-
-    const current_average = running_average(key)
-    const already_thrown_away = values_thrown_away_counter[key]
-    const is_value_out_of_range = Math.abs(current_average - value) >= THRESHOLD_TO_THROW_AWAY
-    const do_we_have_enough_data = values.length >= MIN_VALUES_FOR_RUNNING_AVERAGE_THRESHOLD
-    const throw_away_value = (is_value_out_of_range && do_we_have_enough_data) ?
-        (already_thrown_away >= MAX_VALUES_TO_THROW_AWAY ? false : true) : false
-
-    if (!throw_away_value) {
-        values.push(value)
-
-        if (values.length > VALUES_FOR_RUNNING_AVERAGE) {
-            values.shift()
-        }
-        running_average_map[key] = values
-        values_thrown_away_counter[key] = 0
-    } else {
-        values_thrown_away_counter[key] = values_thrown_away_counter[key] + 1
-        logging.error('throwing away value: ' + value + '   current average: ' + current_average + '  thrown away count: ' + values_thrown_away_counter[key])
-    }
+const average_options = {
+    values_for_running_average: VALUES_FOR_RUNNING_AVERAGE,
+    min_values_for_running_average_threshold: MIN_VALUES_FOR_RUNNING_AVERAGE_THRESHOLD,
+    threshold_to_throw_away: THRESHOLD_TO_THROW_AWAY,
+    max_values_to_throw_away: MAX_VALUES_TO_THROW_AWAY
 }
 
-const running_average = function(key) {
-    var values = running_average_map[key]
-    if (_.isNil(values)) {
-        return 0
-    }
-    var average = 0
-
-    values.forEach(value => {
-        average += value
-
-    })
-
-    return (average / values.length).toFixed(2)
-}
 
 const publish_running_average = function(topic, key, value) {
     if (!_.isNil(value)) {
-        add_running_average(key, value)
-        const average = running_average(key)
+        utilities.add_running_average(key, value, average_options)
+        const average = utilities.running_average(key)
         logging.debug('' + key + ': ' + value)
         logging.debug('  => average: ' + average)
         client.smartPublish(mqtt_helpers.generateTopic(topic, key), average.toString(), mqttOptions)
